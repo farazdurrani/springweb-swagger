a93780b5dfeb60a041b7b682ccc604fa7b053abb
Faraz Durrani <farazkhurram@msn.com>
2020-01-25 17:55:02 -0600

    Attempted to use multiple swaggers to show documentation for particular controllers. Failed.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,43 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("hello.one"))              
+          .paths(PathSelectors.any())                          
+          .build()
+          .groupName("one")
+          .pathMapping("/one");                                           
+    }
+	
+	@Bean
+    public Docket api2() { 
+        return new Docket(DocumentationType.SWAGGER_2)
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("hello.two"))              
+          .paths(PathSelectors.any())                          
+          .build()
+          .groupName("two")
+          .pathMapping("/two");                                           
+    }
+	
+}
